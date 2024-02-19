219f01c0dcb267e54061d8e7717c6843933c716c
Artem-Mullin <mullin.as@phystech.edu>
2024-02-20 02:48:08 +0300

    add layouts

--- app/src/main/java/ru/mullin/cryptoapp/CoinPriceListActivity.kt
+++ app/src/main/java/ru/mullin/cryptoapp/CoinPriceListActivity.kt
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import io.reactivex.disposables.CompositeDisposable
 
-class MainActivity : AppCompatActivity() {
+class CoinPriceListActivity : AppCompatActivity() {
 
     private val compositeDisposable = CompositeDisposable()
     private val viewModel: CoinViewModel by lazy {
@@ -15,7 +15,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_coin_price_list)
         viewModel.priceList.observe(this) {
             Log.d("TEST_OF_LOADING_DATA", "Success in activity")
         }
