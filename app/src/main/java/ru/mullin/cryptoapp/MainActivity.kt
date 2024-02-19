71a59d4972274c033a2bce8458fa7c7528fd3ae5
Artem-Mullin <mullin.as@phystech.edu>
2024-02-20 00:15:25 +0300

    initial commit

---
+++ app/src/main/java/ru/mullin/cryptoapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.mullin.cryptoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
