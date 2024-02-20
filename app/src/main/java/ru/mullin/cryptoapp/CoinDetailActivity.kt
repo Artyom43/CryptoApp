7d62ad6d31be42d778b54d35ede864b7a3a3beda
Artem-Mullin <mullin.as@phystech.edu>
2024-02-20 13:51:02 +0300

    add screen with detail info about coin

---
+++ app/src/main/java/ru/mullin/cryptoapp/CoinDetailActivity.kt
@@ -0,0 +1,47 @@
+package ru.mullin.cryptoapp
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import com.squareup.picasso.Picasso
+import ru.mullin.cryptoapp.databinding.ActivityCoinDetailBinding
+
+class CoinDetailActivity : AppCompatActivity() {
+
+    private val viewModel by lazy { ViewModelProvider(this)[CoinViewModel::class.java] }
+    private val viewBinding by lazy { ActivityCoinDetailBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(viewBinding.root)
+        if (!intent.hasExtra(EXTRA_FROM_SYMBOL)) {
+            finish()
+            return
+        }
+        val fromSymbol = intent.getStringExtra(EXTRA_FROM_SYMBOL)
+        viewModel.getDetailInfo(fromSymbol!!).observe(this) {
+            with(viewBinding) {
+                tvPrice.text = it.price
+                tvMinPrice.text = it.lowDay
+                tvMaxPrice.text = it.highDay
+                tvLastMarket.text = it.lastMarket
+                tvLastUpdate.text = it.getFormattedTime()
+                tvFromSymbol.text = it.fromSymbol
+                tvToSymbol.text = it.toSymbol
+                Picasso.get().load(it.getFullImageUrl()).into(ivLogoCoin)
+            }
+        }
+    }
+
+    companion object {
+        private const val EXTRA_FROM_SYMBOL = "fSym"
+
+        fun newIntent(context: Context, fromSymbol: String): Intent {
+            return Intent(context, CoinDetailActivity::class.java).apply {
+                putExtra(EXTRA_FROM_SYMBOL, fromSymbol)
+            }
+        }
+    }
+}
--- app/src/main/java/ru/mullin/cryptoapp/CoinPriceListActivity.kt
+++ app/src/main/java/ru/mullin/cryptoapp/CoinPriceListActivity.kt
@@ -1,16 +1,12 @@
 package ru.mullin.cryptoapp
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
-import io.reactivex.disposables.CompositeDisposable
 import ru.mullin.cryptoapp.adapter.CoinInfoAdapter
 import ru.mullin.cryptoapp.databinding.ActivityCoinPriceListBinding
 
 class CoinPriceListActivity : AppCompatActivity() {
-
-    private val compositeDisposable = CompositeDisposable()
     private val viewModel: CoinViewModel by lazy {
         ViewModelProvider(this)[CoinViewModel::class.java]
     }
@@ -22,19 +18,13 @@ class CoinPriceListActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         val adapter = CoinInfoAdapter(this) {
-            Log.d("ON_CLICK_TEST", it.toString())
+            val intent = CoinDetailActivity.newIntent(this@CoinPriceListActivity, it.fromSymbol)
+            startActivity(intent)
         }
 
         binding.rvCoinPriceList.adapter = adapter
         viewModel.priceList.observe(this) {
             adapter.coinInfoList = it
         }
-
-
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        compositeDisposable.dispose()
     }
 }
